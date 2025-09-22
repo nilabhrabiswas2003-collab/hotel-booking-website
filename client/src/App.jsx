79b6b822d53c7470cdd53a1a434e0334cbd81378
nilabhrabiswas2003-colab <nilabhrabiswas8@gmail.com>
2025-09-22 21:56:51 +0530

    Adding Testimonial,NewsLetter,Footer,RoomsList,Filter Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import React,{ useState } from 'react'
 import Navbar from './components/Navbar'
 import { Route,Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home';
+import Footer from './components/Footer';
+import AllRooms from './pages/AllRooms';
 
 function App() {
 
@@ -13,8 +15,10 @@ function App() {
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
         </Routes>
       </div>
+      <Footer/>
     </>
   );
 }
--- client/src/components/ExclusiveOffers.jsx
+++ client/src/components/ExclusiveOffers.jsx
@@ -12,16 +12,20 @@ const ExclusiveOffers = () => {
         className='group-hover:translate-x-1 transition-all'/>
         </button>
       </div>
-      <div>
+      <div className='grid grid-cols md:grid-cols lg:grid-cols-3 gap-6 mt-12'>
         {exclusiveOffers.map((item)=>(
-            <div key={item} className='group relative flex flex-col items-start justify-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center'
+            <div key={item._id} className='group relative flex flex-col items-start justify-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center'
             style={{backgroundImage:`url(${item.image})`}}>
                 <p className='px-3 py-1 absolute top-4 left-4 text-xs bg-white text-gray-800 font-medium rounded-full'>{item.priceOff}% Off</p>
                 <div>
                     <p>{item.title}</p>
                     <p>{item.description}</p>
-                    <p>Expires {item.expiryDate}</p>
+                    <p className='text-xs text-white/70 mt-3'>Expires {item.expiryDate}</p>
                 </div>
+                <button className='flex items-center gap-2 font-medium cursor-pointer mt-4 mb-5'>
+                  View Offers
+                  <img className='invert group-hover:translate-x-1 transition-all' src={assets.arrowIcon} alt="arrow-icon"/>
+                </button>
             </div>
         ))}
       </div>
